f08647e1f30986e5d31db4716b014b58e90acddf
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-07-22 16:11:48 +0530

    Login successfully

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 exports.register = async (req, res) => {
@@ -27,4 +28,40 @@ exports.register = async (req, res) => {
       res.status(200).json({ message: `New user created`, user });
     }
   });
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  // check if user exist
+
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+
+    //Compare the password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    //Check to see if there is a match
+    if (!isMatch) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+
+    //Return jsonwebtoken
+    jwt.sign(
+        { _id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: 36000000 },
+      (err, token) => {
+        if (err) {
+          throw err;
+        }
+        res.json({ token, user });
+      }
+    );
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server error");
+  }
 };
--- router/Auth.js
+++ router/Auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { userRegisterValidator } = require('../validators/auth');
+const { userRegisterValidator, userLoginValidator } = require('../validators/auth');
 const { runValidation } = require('../validators/index');
-const { register } = require('../controllers/Auth');
+const { register, login } = require('../controllers/Auth');
 const router = express.Router()
 
 router.post('/register',userRegisterValidator,runValidation, register)
+router.post('/login',userLoginValidator,runValidation, login)
 
 module.exports = router
