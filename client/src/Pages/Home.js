8f1750db667f4deb84c97673f1117f3353b752f5
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-07-23 01:38:29 +0530

    Home page layout

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            Home page
+  return (
+    <div className="home">
+      <div className="card home-card">
+        <h5>Name</h5>
+        <div className="card-image">
+          <img
+            src="https://images.unsplash.com/photo-1509023464722-18d996393ca8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            alt="image"
+          />
         </div>
-    )
-}
+        <div className="card-content">
+        <i className="material-icons" style={{ color: 'red' }}>favorite</i>
+            <h6>title</h6>
+            <p>text of the post</p>
 
-export default Home
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
