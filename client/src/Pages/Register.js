273189daf29ed50f0663ce12cb99dc5f86e0f154
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-07-23 05:12:26 +0530

    Bit of refactoring in register.js

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import SignupForm from "../components/Auth/SignupForm";
+import { useHistory } from 'react-router-dom'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 import { registerUser } from "../helpers/auth";
 
 const Register = () => {
@@ -9,13 +12,23 @@ const Register = () => {
     password: "",
   });
 
+  const history = useHistory()
+
   const onSubmitHandler = (e) => {
     e.preventDefault();
     registerUser({
       name: values.name,
       password: values.password,
       email: values.email
-    }).then((response) => console.log(response));
+    }).then((response) => {
+      if(response.error) {
+        console.log(response.error)
+        toast.error(`${response.error}`)
+      } else {
+        setValues({ ...values, name: '', email: '', password: '' })
+        history.push('/login')
+      }
+    });
   };
 
   const onChangeHandler = (name) => (e) => {
@@ -24,12 +37,12 @@ const Register = () => {
 
   return (
     <>
-      {JSON.stringify(values)}
       <SignupForm
         values={values}
         onSubmitHandler={onSubmitHandler}
         onChangeHandler={onChangeHandler}
       />
+      <ToastContainer />
     </>
   );
 };
--- client/src/helpers/auth.js
+++ client/src/helpers/auth.js
@@ -1,13 +1,31 @@
 import axios from "axios";
-import {API} from './config'
-export const registerUser = (data) => {
-  return axios.post( API + "/auth/register", JSON.stringify(data), {
-    headers: { "Content-Type": "application/json" },
-  }).then(response => {
-      console.log(response)
-      return response.data
-  }).catch(error => {
-      console.log(error.message)
-      return error
-  })
+import { API } from "./config";
+
+export const registerUser = (user) => {
+  return axios
+    .post(`${API}/auth/register`, JSON.stringify(user), {
+      headers: { "Content-Type": "application/json" },
+    })
+    .then((response) => {
+      return response;
+    })
+    .catch((error) => {
+      console.log(error);
+      return error.response.data;
+    });
+};
+
+export const loginUser = (data) => {
+  return axios
+    .post(API + "/auth/login", JSON.stringify(data), {
+      headers: { "Content-Type": "application/json" },
+    })
+    .then((response) => {
+      console.log(response);
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error.message);
+      return error;
+    });
 };
