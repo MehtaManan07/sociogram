04b6c17b72c98022055db0b1af7157f420ed9b47
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-07-22 23:34:14 +0530

    simple Login layout

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from "react";
 
 const Register = () => {
-    return (
-        <div>
-            Register
+  return (
+    <div className="row">
+      <div className="auth-card">
+      <div className="card">
+        <h2>Sociogram</h2>
+        <div className="row">
+        <div className="input-field col s12">
+          <input id="password" type="password" className="validate" />
+          <label for="password">Password</label>
         </div>
-    )
-}
+      </div>
+      <div className="row">
+        <div className="input-field col s12">
+          <input id="email" type="email" className="validate" />
+          <label for="email">Email</label>
+        </div>
+      </div>
+        <button
+          className="btn waves-effect waves-light"
+          type="submit"
+          name="action"
+        >
+          Submit
+          <i className="material-icons right">send</i>
+        </button>
+      </div>
+      </div>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
+
+/* 
+  <div class="row">
+    <div class="col s12 m6">
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+          <span class="card-title">Card Title</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+*/
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav>
-    <div class="nav-wrapper purple lighten-1">
-      <Link to="/" class="brand-logo">Sociogram</Link>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
+    <div className="nav-wrapper purple lighten-1">
+      <Link to="/" className="brand-logo">Sociogram</Link>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
         <li><Link to="/login">Login</Link></li>
         <li><Link to="/register">Register</Link></li>
         <li><Link to="/profile">Profile</Link></li>
