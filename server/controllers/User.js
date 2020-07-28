85a344306458706655ca7b16fc68ef36d8072918
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-07-28 13:25:16 +0530

    Backend for follow and unfollow user

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -3,21 +3,61 @@ const Post = require("../models/Post");
 const router = require("../routes/User");
 
 exports.getUserProfile = (req, res) => {
-  console.log(req.params.id)
-  User.findOne({ _id: req.params.id})
+  console.log(req.params.id);
+  User.findOne({ _id: req.params.id })
     .select("-password")
     .exec((error, user) => {
       if (error || !user) {
-        console.log(error, user)
+        console.log(error, user);
         return res.status(404).json({ error: "User not found" });
       }
       Post.find({ user: req.params.id })
-      .populate("user", "_id name")
-      .exec((error, posts) => {
-          if(error) {
-              return res.status(400).json(error)
+        .populate("user", "_id name")
+        .exec((error, posts) => {
+          if (error) {
+            return res.status(400).json(error);
           }
-          res.json({ user,posts })
-      })
+          res.json({ user, posts });
+        });
     });
+};
+
+exports.followUser = (req, res) => {
+  User.findByIdAndUpdate(
+    req.body.followId,
+    { $push: { followers: req.user._id } },
+    { new: true },
+    (error, result) => {
+      if (error) {
+        return res.status(422).json({ error });
+      }
+      User.findByIdAndUpdate(req.user._id,{ $push: { following: req.body.followId } },
+        { new: true },(error,response) => {
+          if (error) {
+            return res.status(422).json({ error });
+          }
+          res.status(200).json(response)
+        })
+    }
+  );
+};
+
+exports.unfollowUser = (req, res) => {
+  User.findByIdAndUpdate(
+    req.body.unfollowId,
+    { $pull: { followers: req.user._id } },
+    { new: true },
+    (error, result) => {
+      if (error) {
+        return res.status(422).json({ error });
+      }
+      User.findByIdAndUpdate(req.user._id,{ $pull: { following: req.body.unfollowId } },
+        { new: true },(error,response) => {
+          if (error) {
+            return res.status(422).json({ error });
+          }
+          res.status(200).json(response)
+        })
+    }
+  );
 };
--- server/routes/User.js
+++ server/routes/User.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { getUserProfile } = require('../controllers/User');
+const { getUserProfile, followUser, unfollowUser } = require('../controllers/User');
 const  requireLogin  = require("../middlewares/requireLogin")
 const router = express.Router()
 
 router.get("/:id",requireLogin,getUserProfile)
+router.put(`/follow`, requireLogin, followUser)
+router.put(`/unfollow`, requireLogin, unfollowUser)
 
 module.exports = router
