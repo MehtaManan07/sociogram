74bfd6273a5d3d5f5e4422d95d0627d5ac2a8a79
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-07-31 01:13:30 +0530

    Updated user model and removed moment from backend

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const moment = require("moment");
 const { ObjectId } = mongoose.Schema.Types;
 const userSchema = new mongoose.Schema({
   name: {
@@ -16,6 +15,10 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
   bio: {
+    type: String,
+    default: "",
+  },
+  username: {
     type: String,
     default: "",
   },
